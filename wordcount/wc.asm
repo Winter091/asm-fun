3730b42c3863cfc6da6f337d1240458938396c5b
winter091 <winter091@yandex.ru>
2021-04-01 13:54:37 +0300

    Add wordcount

---
+++ wordcount/wc.asm
@@ -0,0 +1,178 @@
+sys_read:   equ     0
+sys_write:  equ     1
+sys_exit:   equ     60
+
+stdin_fileno:   equ     0
+stdout_fileno:  equ     1
+stderr_fileno:  equ     2
+
+; buf is on stack
+buf_size:   equ     8192
+
+; ===================================================
+
+    section     .rodata
+error_msg:      db      "Read error occured", 0x0a
+error_msg_len:  equ     $ - error_msg
+
+; ===================================================
+
+    section     .text
+    global      _start
+
+_start:
+    ; word_count = 0;
+    xor         r9, r9
+
+    ; last_char_was_word = 0;
+    xor         r8,  r8
+
+    ; char buf[8192];
+    sub         rsp, buf_size
+    mov         rsi, rsp
+
+read_next_buf:
+    ; if (last_char_was_word) {
+    ;   word_count++;
+    ;   last_char_was_word = 0;
+    ; }
+    test        r8, r8
+    jz          .continue_reading
+    inc         r9
+    xor         r8, r8
+
+.continue_reading:
+    ; fread(buf, 8192, stdin);
+    xor         rax, rax
+    xor         rdi, rdi
+    mov         rdx, buf_size
+    syscall
+
+    ; if (res == 0)
+    ;   goto quit;
+    ; else if (res < 0)
+    ;   goto read_error;
+    test        rax, rax
+    jz          quit
+    js          read_error
+
+    ; curr_index = 0;
+    xor         rcx, rcx
+
+    ; last_char_was_word = 0;
+    xor         r8,  r8
+
+check_next_char:
+    ; if (curr_index == bytes_read)
+    ;   goto read_next_buf;
+    cmp         rcx, rax
+    je          read_next_buf
+
+    ; if (c < 4 || c > 13)
+    ;   goto check_if_32
+    ; else 
+    ;   goto char_is_space
+    mov         bl, byte [rsi + rcx]
+    sub         rbx, 9
+    cmp         rbx, 4
+    jg          check_if_32         
+    jmp         char_is_space
+
+check_if_32:
+    cmp         rbx, 23
+    jne         char_is_not_space
+
+char_is_space:
+    ; if (last_char_was_word == true)
+    ;   word_count++;
+    test        r8, r8
+    jz          after_increment
+    inc         r9
+
+after_increment:
+    ; last_char_was_word = 0;
+    ; goto loop_end;
+    xor         r8, r8 
+    jmp         loop_end
+
+char_is_not_space:
+    ; last_char_was_word = 1;
+    mov         r8, 1
+
+loop_end:
+    ; curr_index++;
+    inc         rcx
+    jmp         check_next_char
+
+quit:
+    ; print_answer()
+    call        print_int
+    
+    ; exit()
+    mov         rax, sys_exit
+    xor         rdi, rdi
+    syscall     
+
+read_error:
+    ; print("Read error occured\n");
+    mov         rax, sys_write
+    mov         rdi, stdout_fileno
+    mov         rsi, error_msg
+    mov         rdx, error_msg_len
+    syscall
+
+    ; exit()
+    mov         rax, sys_exit
+    xor         rdi, rdi
+    syscall
+
+print_int:
+    ; Allocate char[24] ending with '\n'
+    mov         r11, rsp
+    dec         r11
+    mov         byte [r11], 0x0a
+    sub         rsp, 24
+
+    ; r10 is curr len, r12 is const for div,
+    ; rax holds curr number of words
+    mov         r10, 1
+    mov         r12, 10
+    mov         rax, r9
+    
+.next_digit:
+    ; num_digits++;
+    inc         r10
+
+    ; res /= 10;
+    xor         rdx, rdx 
+    div         r12
+
+    ; buf[--i] = char(remainder);
+    dec         r11
+    add         dl, '0'
+    mov         byte [r11], dl
+
+    ; if (res == 0)
+    ;   goto end
+    ; else
+    ;   goto next_digit
+    test        rax, rax
+    jz          .end
+    jmp         .next_digit
+
+.end:
+    ; Add '\n' to the start of the buffer
+    ; dec         r11
+    ; mov         byte [r11], 0x0a
+    ; inc         r10
+
+    ; print("%d", answer);
+    mov         rax, sys_write
+    mov         rdi, stdout_fileno
+    mov         rsi, r11
+    mov         rdx, r10
+    syscall
+
+    ; free(char[24])
+    add         rsp, 24
+    ret
